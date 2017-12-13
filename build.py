cd176d5b918b45662ba97a0c8bde5ff162b1f29e
isaactsg <idw.wismer@gmail.com>
2017-12-13 13:40:48 -0500

    change to simple build command

--- build.py
+++ build.py
@@ -182,58 +182,10 @@ def WhiteboxGIS():
     subprocess.call(cd + ' bin' + slash + 'WhiteboxGIS' + slash + ' && jar cmf0 ..' + slash + '..' + slash + 'WhiteboxGIS' + slash + 'MANIFEST.MF ..' + slash + 'WhiteboxGIS.jar *', shell=True)
 
 def WhiteboxTools():
-    try:
-        update_cargo = False
-        clean_code = False
-        doc_code = False
-        build_code = True
-        mode = 'release'  # 'check', 'debug', or 'release'
-
-        # Change the current directory
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        os.chdir(dir_path)
-
-        if update_cargo:
-            # Update #
-            retcode = call(['cargo', 'update'], shell=False)
-            if retcode < 0:
-                print >>sys.stderr, "Child was terminated by signal", -retcode
-            else:
-                print >>sys.stderr, "Update successful"
-
-        if clean_code:
-            # Clean #
-            retcode = call(['cargo', 'clean'], shell=False)
-            if retcode < 0:
-                print >>sys.stderr, "Child was terminated by signal", -retcode
-            else:
-                print >>sys.stderr, "Clean successful"
-
-        if doc_code:
-            # Clean #
-            retcode = call(['cargo', 'doc'], shell=False)
-            if retcode < 0:
-                print >>sys.stderr, "Child was terminated by signal", -retcode
-            else:
-                print >>sys.stderr, "Clean successful"
-
-        if build_code:
-            # Build #
-            if mode == 'release':
-                retcode = call(['env', 'RUSTFLAGS=-C target-cpu=native', 'CARGO_INCREMENTAL=1',
-                                'cargo', 'build', '--release'], shell=False)
-            elif mode == 'check':
-                retcode = call(['cargo', 'check'], shell=False)
-            else:
-                retcode = call(['cargo', 'build'], shell=False)
-
-            if retcode < 0:
-                print >>sys.stderr, "Child was terminated by signal", -retcode
-            else:
-                print >>sys.stderr, "Build executed successfully"
-
-    except OSError as err:
-        print >>sys.stderr, "Execution failed:", err
+    subprocess.call(cd  + ' whitebox_tools' + slash, shell=True)
+    subprocess.call('cargo build --release', shell=True);
+    subprocess.call(cd  + ' ..', shell=True)
+ 
 
 def clean():
     print("Cleaning bin/ and release/")
